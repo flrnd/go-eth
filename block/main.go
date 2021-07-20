63056c2b17698d15d95daa9c01a8ced202a524c3
Florian Rand <florian.rand@gmail.com>
2021-07-20 16:58:30 +0200

    Add examples

---
+++ block/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://localhost:8545")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected")
+
+	header, err := client.HeaderByNumber(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blockNumber := big.NewInt(header.Number.Int64())
+
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(block.Number().Uint64())
+	fmt.Println(block.Time())
+	fmt.Println(block.Difficulty().Uint64())
+	fmt.Println(block.Hash().Hex())
+	fmt.Println(len(block.Transactions()))
+}
