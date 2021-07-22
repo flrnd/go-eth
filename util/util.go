416806ab870d16b6e59c974101bf51dc40e4736a
Florian Rand <florian.rand@gmail.com>
2021-07-22 17:46:59 +0200

    Add util package

---
+++ util/util.go
@@ -0,0 +1,85 @@
+package util
+
+import (
+	"math/big"
+	"reflect"
+	"regexp"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/shopspring/decimal"
+)
+
+// IsValidAddress validate hex address
+func IsValidAddress(iaddress interface{}) bool {
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	switch v := iaddress.(type) {
+	case string:
+		return re.MatchString(v)
+	case common.Address:
+		return re.MatchString(v.Hex())
+	default:
+		return false
+	}
+}
+
+// IsZeroAddress validate if it's a 0 address
+func IsZeroAddress(iaddress interface{}) bool {
+	var address common.Address
+
+	switch v := iaddress.(type) {
+	case string:
+		address = common.HexToAddress(v)
+	case common.Address:
+		address = v
+	default:
+		return false
+	}
+
+	zeroAddressBytes := common.FromHex("0x0000000000000000000000000000000000000000") // 0x0(40)
+	addressBytes := address.Bytes()
+
+	return reflect.DeepEqual(addressBytes, zeroAddressBytes)
+}
+
+// ToDecimal wei to decimal
+func ToDecimal(ivalue interface{}, decimals int) decimal.Decimal {
+	value := new(big.Int)
+
+	switch v := ivalue.(type) {
+	case string:
+		value.SetString(v, 10)
+	case *big.Int:
+		value = v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	num, _ := decimal.NewFromString(value.String())
+
+	return num.Div(mul)
+}
+
+// ToWei decimal to wei
+func ToWei(iamount interface{}, decimals int) *big.Int {
+	amount := decimal.NewFromFloat(0)
+
+	switch v := iamount.(type) {
+	case string:
+		amount, _ = decimal.NewFromString(v)
+	case float64:
+		amount = decimal.NewFromFloat(v)
+	case int64:
+		amount = decimal.NewFromFloat(float64(v))
+	case decimal.Decimal:
+		amount = v
+	case *decimal.Decimal:
+		amount = *v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	result := amount.Mul(mul)
+
+	wei := new(big.Int)
+	wei.SetString(result.String(), 10)
+
+	return wei
+}
